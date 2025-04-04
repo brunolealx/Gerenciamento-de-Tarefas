e7684a8948641e05ea99545dbe05affe969430bf
Bruno Leal <lealbruno759@gmail.com>
2025-04-04 12:43:38 -0300

    Atualiza componente App para renderizar lista de tarefas

--- todo/src/app.jsx
+++ todo/src/app.jsx
@@ -1,9 +1,41 @@
 import { useState } from 'react'
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir pra academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ])
 
   return (
-   <div>Olá React</div>  
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <div className="content">
+              <p>{todo.text}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
+
+export default App
